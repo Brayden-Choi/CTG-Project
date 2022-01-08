aad4f2c84843aa3c35f17f7bc59df6f0432ee624
Brayden <97339832+Brayden-Choi@users.noreply.github.com>
2022-01-08 22:46:21 +0800

    Add files via upload

---
+++ ED.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RC4
+{
+    class ED
+    {
+        public static byte[] Encrypt(byte[] pwd, byte[] data)
+        {
+            int a, i, j, m, tbox;
+            int[] key, box;
+            byte[] result;
+
+            key = new int[256];
+            box = new int[256];
+            result = new byte[data.Length];
+
+            for (i = 0; i < 256; i++)
+            {
+                key[i] = pwd[i % pwd.Length];
+                box[i] = i;
+            }
+
+            for (j = i = 0; i < 256; i++)
+            {
+                j = (j + box[i] + key[i]) % 256;
+                tbox = box[i];
+                box[j] = box[i];
+                box[j] = tbox;
+            }
+
+            for (a = j = i = 0; i < data.Length; i++)
+            {
+                a++;
+                a = a%256;
+                j += box[a];
+                j = j%256;
+                tbox = box[a];
+                box[j] = box[a];
+                box[j] = tbox;
+                m = box[((box[a] + box[j]) % 256)];
+                result[i] = (byte)(data[i] ^ m);
+            }
+            return result;
+        }
+
+        public static byte[] Decrypt(byte[] pwd, byte[] data)
+        {
+            return Encrypt(pwd, data);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace RC4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int option = DisplayMenu();
+
+                if (option == 1)
+                {   
+                    Console.Write("Enter Password: ");
+                    int ipwd = Convert.ToInt32(Console.ReadLine());
+                    byte[] pwd = BitConverter.GetBytes(ipwd);
+                    Console.Write("Enter Plaintext: ");
+                    int idata = Convert.ToInt32(Console.ReadLine());
+                    byte[] data = BitConverter.GetBytes(idata);
+                    byte[] result = ED.Encrypt(pwd, data);
+                    int iresult = BitConverter.ToInt32(result);
+                    Console.WriteLine("CipherText: {0}", iresult);
+                }
+
+                else if (option == 2)
+                {
+                    Console.Write("Enter Password: ");
+                    int ipwd = Convert.ToInt32(Console.ReadLine());
+                    byte[] pwd = BitConverter.GetBytes(ipwd);
+                    Console.Write("Enter Ciphertext: ");
+                    int idata = Convert.ToInt32(Console.ReadLine());
+                    byte[] data = BitConverter.GetBytes(idata);
+                    byte[] result = ED.Encrypt(pwd, data);
+                    int iresult = BitConverter.ToInt32(result);
+                    Console.WriteLine("Plaintext: {0}", iresult);
+                }
+
+                else if (option == 0)
+                {
+                    Console.WriteLine("Goodbye, have a nice day!");
+                    break;
+                }
+            }
+        }
+
+        static int DisplayMenu()
+        {
+            Console.WriteLine("---------------- M E N U --------------------");
+            Console.WriteLine("[1] Encrypt your plain text");
+            Console.WriteLine("[2] Decrypt your cipher text");
+            Console.WriteLine("[0] Exit");
+            Console.WriteLine("---------------------------------------------");
+            Console.Write("Enter your option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            return option;
+        }
+    }
+}
